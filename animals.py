e4a0ef9f3fef822756b2e4f37e674a68429bad2d
Val Currie <contact@valcuri.email>
2021-07-13 16:02:02 -0500

    having fun with it

--- animals.py
+++ animals.py
@@ -11,117 +11,119 @@ class Alpaca:
 		self.species = species
 
 class Jackass:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
 		self.walking = True
-		self.name = ""
-		self.species = ""
+		self.name = name
+		self.species = species
+
 
 class Pony:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
 		self.walking = True
-		self.name = ""
-		self.species = ""
+		self.name = name
+		self.species = species
 
 class Goat:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
 		self.walking = True
-		self.name = ""
-		self.species = ""
+		self.name = name
+		self.species = species
 
 class Hedgehog:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
 		self.walking = True
-		self.name = ""
-		self.species = ""
+		self.requiredSpeed = "Fast"
+		self.name = name
+		self.species = species
 
 # SLITHERING
 
 class GiantSnail:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.slithering = True
-		self.name = ""
-		self.species = ""
+		self.walking = True
+		self.name = name
+		self.species = species
 
 class Worm:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.slithering = True
-		self.name = ""
-		self.species = ""
+		self.walking = True
+		self.name = name
+		self.species = species
 
 class Snake:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.slithering = True
-		self.name = ""
-		self.species = ""
+		self.walking = True
+		self.name = name
+		self.species = species
 
-class :
-	def __init__(self):
+class LeglessLizard:
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.slithering = True
-		self.name = ""
-		self.species = "UNKNOWN"
+		self.walking = True
+		self.name = name
+		self.species = species
 
 class MysteriousSlime:
-	def __init__(self):
+	def __init__(self, name):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.slithering = True
-		self.name = ""
-		self.species = ""
+		self.walking = True
+		self.name = name
+		self.species = "UNKNOWN" 
 
 # SWIMMING
 
 class SpottedEel:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.swimming = True
-		self.name = ""
-		self.species = ""
+		self.walking = True
+		self.name = name
+		self.species = species
 
 class Koi:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.swimming = True
-		self.name = ""
-		self.species = ""
+		self.walking = True
+		self.name = name
+		self.species = species
 
 class Trout:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.swimming = True
-		self.name = ""
-		self.species = ""
+		self.walking = True
+		self.name = name
+		self.species = species
 
 class Seahorse:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.swimming = True
-		self.name = ""
-		self.species = ""
+		self.walking = True
+		self.name = name
+		self.species = species
 
 class Piranha:
-	def __init__(self):
+	def __init__(self, name, species):
 		# no specific details by default
 		self.date_added = date.Today()
-		self.swimming = True
-		self.name = ""
-		self.species = ""
+		self.walking = True
+		self.name = name
+		self.species = species
