bbe2a2afbc24f4f60dfc9f8a809b2c34fa28e24c
Val Currie <contact@valcuri.email>
2021-07-13 15:47:32 -0500

    added a critter for each enclosure

---
+++ animals.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+class Alpaca:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.walking = True
+		self.name = ""
+		self.species = ""
+
+class GiantSnail:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.slithering = True
+		self.name = ""
+		self.species = ""
+
+class SpottedEel:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.swimming = True
+		self.name = ""
+		self.species = ""
