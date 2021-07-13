6bbe2ad1c3aa872fadfc6a18901a8eb713468111
Val Currie <contact@valcuri.email>
2021-07-13 15:51:56 -0500

    15 critters and lots of repetition

--- animals.py
+++ animals.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+# WALKING
+
 class Alpaca:
 	def __init__(self):
 		# no specific details by default
@@ -7,6 +9,40 @@ class Alpaca:
 		self.walking = True
 		self.name = ""
 		self.species = ""
+
+class Jackass:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.walking = True
+		self.name = ""
+		self.species = ""
+
+class Pony:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.walking = True
+		self.name = ""
+		self.species = ""
+
+class Goat:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.walking = True
+		self.name = ""
+		self.species = ""
+
+class Hedgehog:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.walking = True
+		self.name = ""
+		self.species = ""
+
+# SLITHERING
 
 class GiantSnail:
 	def __init__(self):
@@ -16,7 +52,73 @@ class GiantSnail:
 		self.name = ""
 		self.species = ""
 
+class Earthworm:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.slithering = True
+		self.name = ""
+		self.species = ""
+
+class Anaconda:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.slithering = True
+		self.name = ""
+		self.species = ""
+
+class LeglessLizard:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.slithering = True
+		self.name = ""
+		self.species = ""
+
+class MysteriousSlime:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.slithering = True
+		self.name = ""
+		self.species = ""
+
+# SWIMMING
+
 class SpottedEel:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.swimming = True
+		self.name = ""
+		self.species = ""
+
+class AlbinoTigerOscar:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.swimming = True
+		self.name = ""
+		self.species = ""
+
+class RainbowTrout:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.swimming = True
+		self.name = ""
+		self.species = ""
+
+class Seahorse:
+	def __init__(self):
+		# no specific details by default
+		self.date_added = date.Today()
+		self.swimming = True
+		self.name = ""
+		self.species = ""
+
+class Piranha:
 	def __init__(self):
 		# no specific details by default
 		self.date_added = date.Today()
