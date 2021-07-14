321843406744a6375b5ecf9d99569bd68b82f220
Val Currie <contact@valcuri.email>
2021-07-14 10:45:52 -0500

    added shifts for walking animals

--- walking_animals/walking_animals.py
+++ walking_animals/walking_animals.py
@@ -1,38 +1,48 @@
 from datetime import date
 
 class Alpaca:
-	def __init__(self, name, species):
+	def __init__(self, name, species, shift):
 		self.date_added = date.Today()
 		self.walking = True
 		self.name = name
 		self.species = species
+		# shift defaults to "unassigned"
+		self.shift = shift or "unassigned"
 
 class Jackass:
-	def __init__(self, name, species):
+	def __init__(self, name, species, shift):
 		self.date_added = date.Today()
 		self.walking = True
 		self.name = name
 		self.species = species
+		# shift defaults to "unassigned"
+		self.shift = shift or "unassigned"
 
 
 class Pony:
-	def __init__(self, name, species):
+	def __init__(self, name, species, shift):
 		self.date_added = date.Today()
 		self.walking = True
 		self.name = name
 		self.species = species
+		# shift defaults to "unassigned"
+		self.shift = shift or "unassigned"
 
 class Goat:
-	def __init__(self, name, species):
+	def __init__(self, name, species, shift):
 		self.date_added = date.Today()
 		self.walking = True
 		self.name = name
 		self.species = species
+		# shift defaults to "unassigned"
+		self.shift = shift or "unassigned"
 
 class Hedgehog:
-	def __init__(self, name, species):
+	def __init__(self, name, species, shift):
 		self.date_added = date.Today()
 		self.walking = True
 		self.requiredSpeed = "Fast"
 		self.name = name
 		self.species = species
+		# shift defaults to "unassigned"
+		self.shift = shift or "unassigned"
