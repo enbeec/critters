80b6d9ff45a5788447a6e136bcac23a126705704
Val Currie <contact@valcuri.email>
2021-07-14 13:30:55 -0500

    three attractions open now

--- varmint_village.py
+++ varmint_village.py
@@ -25,16 +25,29 @@ class Wetlands:
 		self.animals = list()
 
 varmint_village = PettingZoo("Varmint Village")
-
+belly_bay = SnakePit("Belly Bay")
+sunny_shores = Wetlands("Sunny Shores")
 
 # let's get some crowd pleasers in there
-alfie = Alpaca("Alfie", "Huacaya Alpaca", "alpaca feed", "morning")
+alfie = Alpaca("Alfie", "huacaya alpaca", "alpaca feed", "morning")
 varmint_village.animals.append(alfie)
-peter = Pony("Peter", "Shetland Pony", "oats and hay", "midday")
+peter = Pony("Peter", "shetland pony", "oats and hay", "midday")
 varmint_village.animals.append(peter)
-gilbert = Goat("Gilbert", "Mountain Goat", "literally anything", "afternoon")
+gilbert = Goat("Gilbert", "mountain goat", "literally anything", "afternoon")
 varmint_village.animals.append(gilbert)
 
-for attraction in [varmint_village]:
+greg = GiantSnail("Greg", "big ass snail", "leaves and shit like that")
+belly_bay.animals.append(greg)
+walden = Worm("Walden", "earthworm", "")
+
+sally = SpottedEel("Sally", "teal spotted eel", "tiny crabs")
+sunny_shores.animals.append(sally)
+
+# we don't have a list of attractions to iterate so we just create one real quick
+for attraction in [varmint_village, belly_bay, sunny_shores]:
 	for animal in attraction.animals:
-		print(f'You can find {animal.name} the {animal.species} in {varmint_village.attraction_name}')
+		print(f'You can find {animal.name} the {animal.species} in {attraction.attraction_name}')
+
+# separator -- put your test stuff below this
+print()
+greg.feed()
